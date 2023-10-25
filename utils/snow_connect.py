f5fb7a5962e5dd91c8c359cc149f4f391289aa37
barnesim <imogen.barnes@cba.com.au>
2023-10-25 14:43:13 +1100

    adding utils

---
+++ utils/snow_connect.py
@@ -0,0 +1,33 @@
+from typing import Any, Dict
+
+import streamlit as st
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.version import VERSION
+
+
+
+class SnowflakeConnection:
+    def __init__(self):
+        self.connection_parameters = self._get_connection_parameters_from_env()
+        self.session = None
+
+    @staticmethod
+    def _get_connection_parameters_from_env() -> Dict[str, Any]:
+        connection_parameters = {
+            "account": st.secrets["SNOWFLAKE_ACCOUNT"],
+            "user": st.secrets["SNOWFLAKE_USER"],
+            "password": st.secrets["SNOWFLAKE_PASSWORD"],
+            "warehouse": st.secrets["SNOWFLAKE_WAREHOUSE"],
+            "database": st.secrets["SNOWFLAKE_DATABASE"],
+            "schema": st.secrets["SNOWFLAKE_SCHEMA"],
+            "role": st.secrets["SNOWFLAKE_ROLE"],
+        }
+        return connection_parameters
+        
+    
+    def get_session(self):
+        if self.session is None:
+            self.session = Session.builder.configs(self.connection_parameters).create()
+            self.session.sql_simplifier_enabled = True
+        return self.session
+    
---
+++ utils/snowddls.py
@@ -0,0 +1,19 @@
+class Snowddl:
+    def __init__(self):
+        self.ddl_dict = self.load_ddls()
+
+    @staticmethod
+    def load_ddls():
+        ddl_files = {
+            "FDIC": "sql/select_fdic.sql",
+            "FRED FINANCIAL LABOUR": "sql/select_fred_financial_labour.sql",
+            "FRED INTEREST DATA": "sql/select_fred_interest_data.sql",
+            "FRED UNEPLOYMENT": "sql/select_fred_unemployment.sql",
+            "NCUA CREDIT": "sql/ncua_credit.sql",
+        }
+
+        ddl_dict = {}
+        for table_name, file_name in ddl_files.items():
+            with open(file_name, "r") as f:
+                ddl_dict[table_name] = f.read()
+        return ddl_dict
