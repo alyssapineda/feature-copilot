60904e7a4dafb19a35b40ef738a5c1df15fe7e57
alyssa <110448688+alyssapineda@users.noreply.github.com>
2024-06-23 13:47:43 +1000

    update template with full database name

--- template.py
+++ template.py
@@ -27,19 +27,19 @@ Guidelines:
 
 Here are some examples for FINANCIAL__ECONOMIC_ESSENTIALS database:
 
-        "question": Calculate the total quarterly net income for the banking industry over the last four decades.
+        "question": Aggregate financial data for the banking industry. Calculate the total quarterly net income for the banking industry over the last four decades.
         "answer": SELECT att.variable_name,
                         ts.date,
                         ts.value
-                  FROM cybersyn.financial_fred_timeseries AS ts
-                  JOIN cybersyn.financial_fred_attributes AS att ON (ts.variable = att.variable)
+                  FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_fred_timeseries AS ts
+                  JOIN FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_fred_attributes AS att ON (ts.variable = att.variable)
                   WHERE att.variable_name = 'Income and Expense: Net Income (Loss) Attributable to Bank, Not seasonally adjusted, Quarterly, USD'
                   AND ts.date >= '1984-01-01';
         
         "question": Determine which banks have the highest percentage of uninsured deposits.
         "answer": WITH big_banks AS (
                         SELECT id_rssd
-                        FROM cybersyn.financial_institution_timeseries
+                        FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_institution_timeseries
                         WHERE variable = 'ASSET'
                           AND date = '2022-12-31'
                           AND value > 1E10
@@ -47,9 +47,9 @@ Here are some examples for FINANCIAL__ECONOMIC_ESSENTIALS database:
                   SELECT name,
                           1 - value AS pct_uninsured,
                           ent.is_active
-                  FROM cybersyn.financial_institution_timeseries AS ts
-                  INNER JOIN cybersyn.financial_institution_attributes AS att ON (ts.variable = att.variable)
-                  INNER JOIN cybersyn.financial_institution_entities AS ent ON (ts.id_rssd = ent.id_rssd)
+                  FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_institution_timeseries AS ts
+                  INNER JOIN FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_institution_attributes AS att ON (ts.variable = att.variable)
+                  INNER JOIN FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_institution_entities AS ent ON (ts.id_rssd = ent.id_rssd)
                   INNER JOIN big_banks ON (big_banks.id_rssd = ts.id_rssd)
                   WHERE ts.date = '2022-12-31'
                         AND att.variable_name = '% Insured (Estimated)'
@@ -59,61 +59,37 @@ Here are some examples for FINANCIAL__ECONOMIC_ESSENTIALS database:
         "question": Count credit card complaints by month by company since 2012.
         "answer": SELECT company,
                         DATE_TRUNC('month', date_received) AS month,
-                        COUNT(1) AS credit_card_complaint
-                  FROM cybersyn.financial_cfpb_complaint
+                        COUNT(1)                           AS credit_card_complaint
+                  FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_cfpb_complaint
                   WHERE product ILIKE '%card%'
                         AND date_received >= '2012-01-01'
                   GROUP BY company, month;
-
-        "question": Time series of the effective federal funds effective rate and trimmed mean PCE.
-        "answer": SELECT variable_name,
-                        date,
-                        value
-                  FROM cybersyn.financial_fred_timeseries
-                  WHERE variable_name IN ('Federal Funds Effective Rate',
-                        'Trimmed Mean PCE Inflation Rate, Seasonally adjusted, Monthly, Percent Change YoY')
-                        AND date >= '2019-01-01'
-                  ORDER BY date;
 
         "question": Compare the growth/decline of bank branches by state since the start of the pandemic.
         "answer": WITH pre_covid AS (
                         SELECT state_abbreviation,
                               COUNT(*) AS pre_covid_count
-                        FROM cybersyn.financial_branch_entities
+                        FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_branch_entities
                         WHERE start_date <= '2020-03-01'
                               AND (end_date >= '2020-03-01' OR end_date IS NULL)
                         GROUP BY state_abbreviation
                         )
-                        SELECT cur.state_abbreviation,
-                              pre_covid_count,
-                              COUNT(*)                            AS current_count,
-                              current_count / pre_covid_count - 1 AS pct_change
-                        FROM cybersyn.financial_branch_entities AS cur
-                        INNER JOIN pre_covid ON (cur.state_abbreviation = pre_covid.state_abbreviation)
-                        WHERE end_date IS NULL
-                        GROUP BY cur.state_abbreviation, pre_covid_count
-                        ORDER BY pct_change;
+                  SELECT cur.state_abbreviation,
+                        pre_covid_count,
+                        COUNT(*)                            AS current_count,
+                        current_count / pre_covid_count - 1 AS pct_change
+                  FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_branch_entities AS cur
+                  INNER JOIN pre_covid ON (cur.state_abbreviation = pre_covid.state_abbreviation)
+                  WHERE end_date IS NULL
+                  GROUP BY cur.state_abbreviation, pre_covid_count
+                  ORDER BY pct_change;
       
         "question": Average price paid for a typical basket of goods purchased at a convenience store. Create a time series of the average price paid for bananas, soft drinks, and ice cream nationwide starting in 2021
         "answer": SELECT ts.date,
                         att.variable_name,
                         ts.value
-                  FROM cybersyn.bureau_of_labor_statistics_price_timeseries AS ts
-                  JOIN cybersyn.bureau_of_labor_statistics_price_attributes AS att
-                        ON (ts.variable = att.variable)
-                  WHERE ts.date >= '2021-01-01'
-                        AND att.report = 'Average Price'
-                        AND (att.product ILIKE 'Ice cream%' OR
-                              att.product ILIKE 'Bananas%' OR
-                              att.product ILIKE 'All soft drinks, per 2 liters%')
-                  ORDER BY date;
-
-        "question": Average price paid for a typical basket of goods purchased at a convenience store. Create a time series of the average price paid for bananas, soft drinks, and ice cream nationwide starting in 2021
-        "answer": SELECT ts.date,
-                        att.variable_name,
-                        ts.value
-                  FROM cybersyn.bureau_of_labor_statistics_price_timeseries AS ts
-                  JOIN cybersyn.bureau_of_labor_statistics_price_attributes AS att
+                  FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.bureau_of_labor_statistics_price_timeseries AS ts
+                  JOIN FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.bureau_of_labor_statistics_price_attributes AS att
                         ON (ts.variable = att.variable)
                   WHERE ts.date >= '2021-01-01'
                         AND att.report = 'Average Price'
@@ -126,7 +102,7 @@ Here are some examples for FINANCIAL__ECONOMIC_ESSENTIALS database:
         "answer": SELECT variable_name,
                         date,
                         value
-                  FROM cybersyn.financial_fred_timeseries
+                  FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_fred_timeseries
                   WHERE variable_name IN ('Federal Funds Effective Rate',
                                           'Trimmed Mean PCE Inflation Rate, Seasonally adjusted, Monthly, Percent Change YoY')
                         AND date >= '2019-01-01'
@@ -136,8 +112,8 @@ Here are some examples for FINANCIAL__ECONOMIC_ESSENTIALS database:
         "answer": SELECT ts.date,
                         ts.variable_name,
                         ts.value
-                  FROM cybersyn.financial_fred_timeseries AS ts
-                  JOIN cybersyn.financial_fred_attributes AS att
+                  FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_fred_timeseries AS ts
+                  JOIN FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_fred_attributes AS att
                         ON (att.variable = ts.variable)
                   WHERE variable_group IN ('Bank of Brazil Selic Interest Rate Target',
                                           'Bank of Canada Overnight Lending Rate',
@@ -151,10 +127,10 @@ Here are some examples for FINANCIAL__ECONOMIC_ESSENTIALS database:
         "answer": SELECT geo.geo_name,
                         att.variable_name,
                         ts.date, ts.value
-                  FROM cybersyn.bureau_of_labor_statistics_employment_timeseries AS ts
-                  JOIN cybersyn.bureau_of_labor_statistics_employment_attributes AS att
+                  FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.bureau_of_labor_statistics_employment_timeseries AS ts
+                  JOIN FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.bureau_of_labor_statistics_employment_attributes AS att
                         ON (ts.variable = att.variable)
-                  JOIN cybersyn.geography_index AS geo
+                  JOIN FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.geography_index AS geo
                         ON (ts.geo_id = geo.geo_id)
                   WHERE att.report = 'State and Metro Employment'
                         AND att.industry = 'Financial Activities'
@@ -172,14 +148,14 @@ Here are some examples for FINANCIAL__ECONOMIC_ESSENTIALS database:
                         value,
                         (value / LAG(value, 12) OVER (PARTITION BY geo_id, variable ORDER BY date) - 1) * 100 AS yoy_change,
                         unit
-                  FROM cybersyn.financial_fred_timeseries
+                  FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.financial_fred_timeseries
                   WHERE variable_name = 'Personal Consumption Expenditures: Chain-type Price Index, Seasonally adjusted, Monthly, Index 2017=100')
 
                   SELECT *
                   FROM cte
                   WHERE date >= '2019-01-01';
 
-        "question": Find YTD performance of a select group of stocks. YTD performance of the Magnificent 7.
+        "question": Find Year-to-date / YTD performance of a select group of stocks. YTD performance of the Magnificent 7.
         "answer": WITH ytd_performance AS (
                         SELECT
                               ticker,
@@ -187,7 +163,7 @@ Here are some examples for FINANCIAL__ECONOMIC_ESSENTIALS database:
                               FIRST_VALUE(value) OVER (PARTITION BY ticker ORDER BY date ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS start_of_year_price,
                               MAX(date) OVER (PARTITION BY ticker) AS latest_date,
                               LAST_VALUE(value) OVER (PARTITION BY ticker ORDER BY date ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_price
-                        FROM cybersyn.stock_price_timeseries
+                        FROM FINANCIAL__ECONOMIC_ESSENTIALS.cybersyn.stock_price_timeseries
                         WHERE
                               ticker IN ('AAPL', 'MSFT', 'AMZN', 'GOOGL', 'META', 'TSLA', 'NVDA')
                               AND date >= DATE_TRUNC('YEAR', CURRENT_DATE()) -- Truncates the current date to the start of the year
@@ -206,12 +182,13 @@ Here are some examples for FINANCIAL__ECONOMIC_ESSENTIALS database:
                               ticker, start_of_year_date, start_of_year_price, latest_date, latest_price
                         ORDER BY percentage_change_ytd DESC;
                         ;
+
 Here are some examples for SEC_FILINGS database:
 
         "question": Compare financial metrics across companies. Compare revenue numbers from for 2022 for different airline companies.
         "answer": SELECT i.cik, i.company_name, r.period_start_date, r.period_end_date, r.measure_description, TO_NUMERIC(r.value) AS value
                   FROM sec_filings.cybersyn.sec_cik_index AS i
-                  JOIN cybersyn.sec_report_attributes AS r ON (r.cik = i.cik)
+                  JOIN SEC_FILINGS.cybersyn.sec_report_attributes AS r ON (r.cik = i.cik)
                   WHERE i.sic_code_description = 'AIR TRANSPORTATION, SCHEDULED'
                         AND r.statement = 'Income Statement'
                         AND r.period_end_date = '2022-12-31'
@@ -221,30 +198,30 @@ Here are some examples for SEC_FILINGS database:
 
         "question": View historical trends for companies. Measure Chipotle's store count growth over time
         "answer": SELECT i.cik, i.company_name, r.period_end_date, r.measure_description, MAX(TO_NUMBER(r.value)) AS value
-                  FROM cybersyn.sec_cik_index AS i
-                  JOIN cybersyn.sec_report_attributes AS r ON (r.cik = i.cik)
+                  FROM SEC_FILINGS.cybersyn.sec_cik_index AS i
+                  JOIN SEC_FILINGS.cybersyn.sec_report_attributes AS r ON (r.cik = i.cik)
                   WHERE company_name = 'CHIPOTLE MEXICAN GRILL INC'
                   AND r.measure_description = 'Number of restaurants'
                   GROUP BY i.cik, i.company_name, i.cik, r.period_end_date, r.measure_description;
 
         "question": Pull fiscal calendars for public companies. Pull Walmart's fiscal calendar does not align with the calendar year. Pull their quarter start and end dates.
         "answer": SELECT company_name, fiscal_year, fiscal_period, period_start_date, period_end_date
-                  FROM cybersyn.sec_fiscal_calendars
+                  FROM SEC_FILINGS.cybersyn.sec_fiscal_calendars
                   WHERE company_name = 'WALMART INC.'
                   ORDER BY period_end_date;
 
         "question": Track portfolios of fund managers. See Berkshire Hathaway’s most recent public holdings disclosure and join associated tickers under which each holding may be traded globally on all exchanges.
         "answer": WITH latest_filing AS (
                         SELECT adsh
-                        FROM cybersyn.sec_holding_filing_index
+                        FROM SEC_FILINGS.cybersyn.sec_holding_filing_index
                         WHERE filing_manager_name = 'Berkshire Hathaway Inc'
                         ORDER BY filing_date DESC
                         LIMIT 1
                   )
                   SELECT att.*,
                         securities.global_tickers
-                  FROM cybersyn.sec_holding_filing_attributes AS att
-                  LEFT JOIN cybersyn.openfigi_security_index AS securities
+                  FROM SEC_FILINGS.cybersyn.sec_holding_filing_attributes AS att
+                  LEFT JOIN SEC_FILINGS.cybersyn.openfigi_security_index AS securities
                         ON att.top_level_openfigi_id = securities.top_level_openfigi_id
                   WHERE att.adsh IN (SELECT * FROM latest_filing)
                   ORDER BY att.market_value DESC;
@@ -260,12 +237,12 @@ Here are some examples for SEC_FILINGS database:
                         openfigi.openfigi_share_class_id,
                         openfigi.primary_ticker,
                         idx.permid_company_id
-                  FROM cybersyn.company_index AS idx
-                  JOIN cybersyn.company_characteristics AS char
+                  FROM SEC_FILINGS.cybersyn.company_index AS idx
+                  JOIN SEC_FILINGS.cybersyn.company_characteristics AS char
                         ON (idx.company_id = char.company_id)
-                  JOIN cybersyn.company_security_relationships AS rship
+                  JOIN SEC_FILINGS.cybersyn.company_security_relationships AS rship
                         ON (idx.company_id = rship.company_id)
-                  JOIN cybersyn.openfigi_security_index AS openfigi
+                  JOIN SEC_FILINGS.cybersyn.openfigi_security_index AS openfigi
                         ON ARRAY_CONTAINS(rship.security_id::VARIANT, openfigi.openfigi_share_class_id)
                   WHERE char.relationship_type = 'sic_description'
                         AND char.value = 'Air transportation, scheduled';
